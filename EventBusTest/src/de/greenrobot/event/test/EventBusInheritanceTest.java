bdead7d2c835bee93bcde99ada994c2618a26855
Markus Junginger <markus@greenrobot>
2012-07-16 16:28:39 +0200

    fixed old package name in test project

--- EventBusTest/src/de/greenrobot/event/test/EventBusInheritanceTest.java
+++ EventBusTest/src/de/greenrobot/event/test/EventBusInheritanceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.greenrobot.greenbus.test;
+package de.greenrobot.event.test;
 
 import junit.framework.TestCase;
 import de.greenrobot.event.EventBus;
--- EventBusTest/src/de/greenrobot/event/test/EventBusMultithreadedTest.java
+++ EventBusTest/src/de/greenrobot/event/test/EventBusMultithreadedTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.greenrobot.greenbus.test;
+package de.greenrobot.event.test;
 
 import java.util.ArrayList;
 import java.util.List;
