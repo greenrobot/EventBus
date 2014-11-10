0e324bd5d44e9625506333d05759a2d9d556d692
Markus <markus@greenrobot>
2014-11-10 10:48:07 +0100

    eventInheritance disclaimer

--- EventBus/src/de/greenrobot/event/EventBusBuilder.java
+++ EventBus/src/de/greenrobot/event/EventBusBuilder.java
@@ -76,8 +76,12 @@ public class EventBusBuilder {
 
     /**
      * By default, EventBus considers the event class hierarchy (subscribers to super classes will be notified).
-     * Switching this feature off will improve posting of events. For simple event classes extending Object directly, we
-     * measured a speed up of 20%. For more complex event hierarchies, the speed up should be >20%.
+     * Switching this feature off will improve posting of events. For simple event classes extending Object directly,
+     * we measured a speed up of 20% for event posting. For more complex event hierarchies, the speed up should be
+     * >20%.
+     * <p/>
+     * However, keep in mind that event posting usually consumes just a small proportion of CPU time inside an app,
+     * unless it is posting at high rates, e.g. hundreds/thousands of events per second.
      */
     public EventBusBuilder eventInheritance(boolean eventInheritance) {
         this.eventInheritance = eventInheritance;
