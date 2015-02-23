0c89485f73911d57a0d2a6d33c17733e92a8bd02
Markus <markus@greenrobot>
2015-02-23 15:40:41 +0100

    more checks for EventBusAnnotationProcessor

--- EventBusAnnotationProcessor/src/de/greenrobot/event/annotationprocessor/EventBusAnnotationProcessor.java
+++ EventBusAnnotationProcessor/src/de/greenrobot/event/annotationprocessor/EventBusAnnotationProcessor.java
@@ -6,11 +6,15 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.io.BufferedWriter;
@@ -19,6 +23,7 @@ import java.util.List;
 import java.util.Set;
 
 @SupportedAnnotationTypes("de.greenrobot.event.Subscribe")
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class EventBusAnnotationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
@@ -52,17 +57,42 @@ public class EventBusAnnotationProcessor extends AbstractProcessor {
 
     private void writeIndexEntry(BufferedWriter writer, Element element, Messager messager) throws IOException {
         Subscribe subscribe = element.getAnnotation(Subscribe.class);
-        String subscriberClass = element.getEnclosingElement().asType().toString();
-        messager.printMessage(Diagnostic.Kind.NOTE, "Found @Subscribe in " + subscriberClass);
+        if (element.getModifiers().contains(Modifier.STATIC)) {
+            messager.printMessage(Diagnostic.Kind.ERROR, "Subscriber method must not be static", element);
+            return;
+        }
+
+        Set<Modifier> subscriberClassModifiers = element.getEnclosingElement().getModifiers();
+        if (!subscriberClassModifiers.contains(Modifier.PUBLIC)) {
+            messager.printMessage(Diagnostic.Kind.ERROR, "Subscriber class must be public",
+                    element.getEnclosingElement());
+            return;
+        }
+
         List<? extends VariableElement> parameters = ((ExecutableElement) element).getParameters();
         if (parameters.size() != 1) {
-            throw new RuntimeException("Must have exactly 1 parameter");
+            messager.printMessage(Diagnostic.Kind.ERROR, "Subscriber method must have exactly 1 parameter", element);
+            return;
         }
-        String eventType = parameters.get(0).asType().toString();
+
+        VariableElement param = parameters.get(0);
+        DeclaredType paramType = (DeclaredType) param.asType();
+        Set<Modifier> eventClassModifiers = paramType.asElement().getModifiers();
+        if (!eventClassModifiers.contains(Modifier.PUBLIC)) {
+            messager.printMessage(Diagnostic.Kind.ERROR, "Event type must be public: " + paramType, param);
+            return;
+        }
+
+        String subscriberClass = element.getEnclosingElement().asType().toString();
+        Name methodName = element.getSimpleName();
         writer.append("        new SubscriberIndexEntry(\n");
         writer.append("            ").append(subscriberClass).append(".class,\n");
-        writer.append("            \"").append(element.getSimpleName()).append("\",\n");
-        writer.append("            ").append(eventType).append(".class,\n");
+        writer.append("            \"").append(methodName).append("\",\n");
+        writer.append("            ").append(paramType.toString()).append(".class,\n");
         writer.append("            ThreadMode.").append(subscribe.threadMode().name()).append("),\n");
+
+        messager.printMessage(Diagnostic.Kind.NOTE, "Indexed @Subscribe at " +
+                element.getEnclosingElement().getSimpleName() + "." + methodName +
+                "(" + paramType.asElement().getSimpleName() + ")");
     }
 }
