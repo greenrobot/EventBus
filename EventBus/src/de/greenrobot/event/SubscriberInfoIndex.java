976ebbce1c5bd30c2faa8f62ec972ff580ca65d0
Markus <markus@greenrobot>
2015-12-04 16:17:51 +0100

    added SubscriberInfoIndex interface and generated implementation skeleton

---
+++ EventBus/src/de/greenrobot/event/SubscriberInfoIndex.java
@@ -0,0 +1,8 @@
+package de.greenrobot.event;
+
+/**
+ * Interface for generated indexes.
+ */
+public interface SubscriberInfoIndex {
+    SubscriberInfo getSubscriberInfo(Class<?> subscriberClass);
+}
