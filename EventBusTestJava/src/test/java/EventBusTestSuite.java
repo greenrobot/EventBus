6dbbd767bb1fc754f76ba682709a567d9975ddc0
Markus <markus@greenrobot>
2017-09-16 14:06:14 +0100

    Gradle does not pick up tests in src dir, so help Gradle with a suite

---
+++ EventBusTestJava/src/test/java/EventBusTestSuite.java
@@ -0,0 +1,13 @@
+import org.greenrobot.eventbus.EventBusBuilderTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+// Gradle does not pick up tests in src dir, so help Gradle with a suite
+// (Tests are in main src so we can use them for Android too)
+@RunWith(Suite.class)
+@SuiteClasses({
+        EventBusBuilderTest.class
+})
+public class EventBusTestSuite {
+}
