0e32fcea992a0a1159033d0c3bbaf160707756c3
greenrobot Team <opensource+team@greenrobot>
2017-11-14 13:42:44 +0100

    EventBusPerformance: add MAIN_ORDERED.
    - Remove redundant findViewById casts.

--- EventBusPerformance/src/org/greenrobot/eventbusperf/TestRunnerActivity.java
+++ EventBusPerformance/src/org/greenrobot/eventbusperf/TestRunnerActivity.java
@@ -42,7 +42,7 @@ public class TestRunnerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_runtests);
-        textViewResult = (TextView) findViewById(R.id.textViewResult);
+        textViewResult = findViewById(R.id.textViewResult);
         controlBus = new EventBus();
         controlBus.register(this);
     }
